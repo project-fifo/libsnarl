a3abe49eef52096bddead93a076d8623673986f5
Heinz N. Gies <heinz@licenser.net>
2012-08-21 08:27:25 +0200

    First example.

--- old/src/libsnarl_sup.erl
+++ old/src/libsnarl_sup.erl
@@ -24,5 +24,5 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, [?CHILD(libsnarl_server, worker)]} }.
+    {ok, { {one_for_one, 5, 10}, []} }.
 
